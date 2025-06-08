0d810cf0c6a1bad34e1bc516342fefe634a4724e
meerreelly <dimakuryshko20@gmail.com>
2025-06-08 12:45:57 +0000

    Лабораторна 11. Колекції

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Blog\Admin\CategoryController;
 use App\Http\Controllers\Blog\Admin\PostController;
 use App\Http\Controllers\RestTestController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\DiggingDeeperController;
 Route::get('/', function () {
     return view('welcome');
 });
@@ -21,6 +21,10 @@ Route::middleware([
 Route::resource('rest', RestTestController::class)->names('restTest');
 Route::group([ 'namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], function () {
     Route::resource('posts', PostController::class)->names('blog.posts');
+});
+Route::group(['prefix' => 'digging_deeper'], function () {
+    Route::get('collections', [DiggingDeeperController::class, 'collections'])
+        ->name('digging_deeper.collections');
 });
 $groupData = [
     'namespace' => 'App\Http\Controllers\Blog\Admin',
