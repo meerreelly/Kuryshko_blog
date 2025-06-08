7c581d636410159aaa05ef9f1bfd53ab3b3d183d
meerreelly <dimakuryshko20@gmail.com>
2025-06-08 12:28:02 +0000

    Лабораторна 9. Observer перенесення логіки з контроллерів

--- app/Http/Controllers/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -53,9 +53,6 @@ class CategoryController extends BaseController
     {
         //dd(__METHOD__);
         $data = $request->input(); //отримаємо масив даних, які надійшли з форми
-        if (empty($data['slug'])) { //якщо псевдонім порожній
-            $data['slug'] = Str::slug($data['title']); //генеруємо псевдонім
-        }
 
         $item = (new BlogCategory())->create($data); //створюємо об'єкт і додаємо в БД
 
@@ -105,9 +102,6 @@ class CategoryController extends BaseController
         }
 
         $data = $request->all(); //отримаємо масив даних, які надійшли з форми
-        if (empty($data['slug'])) { //якщо псевдонім порожній
-            $data['slug'] = Str::slug($data['title']); //генеруємо псевдонім
-        }
 
         $result = $item->update($data);  //оновлюємо дані об'єкта і зберігаємо в БД
 
