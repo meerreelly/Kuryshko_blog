59429be5b7a6373271454bf534e55b0b6b2203bc
meerreelly <dimakuryshko20@gmail.com>
2025-06-11 11:46:43 +0000

    Лабораторна 13. Ланцюги завдань (Chain)

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,10 @@ Route::group([ 'namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], 
 Route::group(['prefix' => 'digging_deeper'], function () {
     Route::get('collections', [DiggingDeeperController::class, 'collections'])
         ->name('digging_deeper.collections');
+    Route::get('process-video',  [DiggingDeeperController::class, 'processVideo'])
+        ->name('digging_deeper.processVideo');
+    Route::get('prepare-catalog', [DiggingDeeperController::class, 'prepareCatalog'])
+        ->name('digging_deeper.prepareCatalog');
 });
 $groupData = [
     'namespace' => 'App\Http\Controllers\Blog\Admin',
