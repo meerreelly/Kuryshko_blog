4cde7413816cc2f322d45d3ac0d69669c2ce8e02
merely <128153965+meerreelly@users.noreply.github.com>
2025-06-23 14:50:18 +0300

    fix: fix PostController

--- resources/views/blog/admin/posts/index.blade.php
+++ resources/views/blog/admin/posts/index.blade.php
@@ -26,7 +26,7 @@
                                 <tr @if (!$post->is_published) style="background-color: #ccc;" @endif>
                                     <td>{{ $post->id }}</td>
                                     <td>{{ $post->user->name }}</td>    {{-- виводимо ім'я користувача і назву категорії зі зв'язаних таблиць --}}
-                                    <td>{{ $post->category->title }}</td>
+                                    <td>{{ $post->category ? $post->category->title: ''  }}</td>
                                     <td><a href="{{ route('blog.admin.posts.edit', $post->id) }}">{{ $post->title }}</a></td>
                                     <td>{{ $post->published_at ? \Carbon\Carbon::parse($post->published_at)->format('d.M H:i') : '' }}
                                     </td>
